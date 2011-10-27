86792502454c6eda235d525e708801fb84570f0d
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2011-10-27 10:58:29 +0900

    initial commit

---
+++ lib/Test/Fake/HTTPD.pm
@@ -0,0 +1,35 @@
+package Test::Fake::HTTPD;
+
+use 5.008_001;
+use Mouse;
+
+our $VERSION = '0.01';
+
+1;
+
+=head1 NAME
+
+Test::Fake::HTTPD
+
+=head1 SYNOPSIS
+
+    use Test::Fake::HTTPD;
+
+=head1 DESCRIPTION
+
+Test::Fake::HTTPD is
+
+=head1 METHODS
+
+=head1 AUTHOR
+
+NAKAGAWA Masaki E<lt>masaki@cpan.orgE<gt>
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
